20485f2bbbdac66bedbd89b658c64046a3bd43b2
gal <tanjhu@163.com>
2020-05-13 17:10:27 +0800

    實現了隨機顯示unqiue對象用sample,且可以默認顯示或者指點顯示個數

--- tizza/pizza/urls.py
+++ tizza/pizza/urls.py
@@ -1,9 +1,11 @@
 from django.urls import include,path
-from .views import index,random
+from .views import index,random,randoms
 
 
 urlpatterns = [
   path('<int:pid>/', index, name='pizza'),
   path('random/', random, name='random'),
+  path('randoms/', randoms, name='randoms'),
+  path('randoms/<int:random_times>', randoms, name='randoms'),
 
 ]
--- tizza/pizza/views.py
+++ tizza/pizza/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from .models import Pizza
-from random import randint
+from random import randint,sample
 
 def index(request, pid):
     try:
@@ -16,6 +16,27 @@ def random(request):
         return HttpResponse( f" <p>id: {pizza.id}</p> <p>title: {pizza.title}</p><p> description: {pizza.description}</p>" )
     except:
         return HttpResponse(f"<p>status: error</p> <p>message: pizza_id={pid} not found</p>")
+
+def randoms(request,random_times=10):
+    text = ""
+    object_nums = len(Pizza.objects.all())
+    # pid_list = set( [sample(1,object_nums) for i in range (random_times)])
+    if random_times>object_nums:
+        text+=f"您選擇的隨機的次數{random_times}!<br>大於當前存在的pizza數量{object_nums},<br>故只隨機顯示{object_nums}個pizza!!"
+        random_times =object_nums
+    pid_list =  sample(range(1,object_nums+1),random_times)
+    for  i in pid_list:
+        try:
+            pizza = Pizza.objects.get(id=i)
+            text += f" <p>id: {pizza.id}</p> <p>title: {pizza.title}</p><p> description: {pizza.description}</p><br><br>" 
+        except:
+            text +=f"<p>id:{i}</p><p>status: error</p> <p>message: pizza_id={i} not found</p><br><br>"
+    return HttpResponse(text)
+
+    
+
+
+    
 
     
 
