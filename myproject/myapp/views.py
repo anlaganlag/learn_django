b013a73feb77953f05471bf0b769f0226c2b8359
gal <tanjhu@163.com>
2020-05-13 14:28:38 +0800

    tizza項目

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -10,6 +10,8 @@ def hello(request):
 def viewArticle(request, articleId=None,year=None,month=None):
     if  articleId:
         text = f"Displaying article Number : {articleId}"
-    else:
+    elif 1000<year<=2038 and 0<=month<=12:
         text = f"Displaying articles of year month: {year}/{month}/"
+    else:
+        text = f" Input  Arguments : {year}/{month}/"
     return HttpResponse(text)
