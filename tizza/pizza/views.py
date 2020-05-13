18265cdafb065f4957bd23fec3dcd1348272dc42
gal <tanjhu@163.com>
2020-05-13 21:49:20 +0800

    save ;

--- tizza/pizza/views.py
+++ tizza/pizza/views.py
@@ -1,6 +1,49 @@
+import json
+
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from .models import Pizza
 from random import randint,sample
+
+
+from django.shortcuts import render
+from django.views import View
+
+
+class GetTenPizzasView(View):
+    template_name = 'ten_pizzas.html'
+
+    def get(self, request)
+        pizzas = Pizza.objects.order_by('?')[:10]
+        return render(request, self.template_name, {'pizzas': pizzas})
+
+
+@login_required
+def home(request,pid):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        new_pizza = Pizza.objects.create(
+            titile = data['title'],
+            description = data['description'],
+            creator = request.user,
+        )
+        new_pizza.save()
+        return HttpResponse( f" <p>id: {new_pizza.id}</p> <p>title: {new_pizza.title}</p><p> description: {new_pizza.description}</p>" )
+    elif request.method == 'GET':
+        pizza = Pizza.objects.get(id=pid)
+        return HttpResponse( f" <p>id: {pizza.id}</p> <p>title: {pizza.title}</p><p> description: {pizza.description}</p>" )
+    elif request.method == 'DELETE':
+        if 'can_delete' in request.user.user_permissions:
+            pizza = Pizza.objects.get(id=pid)
+            pizza.delete()
+            return HttpResponse( content={
+                'id': pizza.id,
+            })
+        else:
+            return HttpResponse(status_code=404)
+
+    
+
 
 def index(request, pid):
     try:
