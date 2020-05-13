60ddf0bce7ed9d1e6d604492e5a1725691d83ec1
gal <tanjhu@163.com>
2020-05-13 16:15:44 +0800

    增加了一個random_id的功能

--- tizza/pizza/urls.py
+++ tizza/pizza/urls.py
@@ -1,8 +1,9 @@
 from django.urls import include,path
-from .views import index
+from .views import index,random
 
 
 urlpatterns = [
   path('<int:pid>/', index, name='pizza'),
+  path('random/', random, name='random'),
 
 ]
--- tizza/pizza/views.py
+++ tizza/pizza/views.py
@@ -1,10 +1,23 @@
 from django.http import HttpResponse
 from .models import Pizza
+from random import randint
 
 def index(request, pid):
-    pizza = Pizza.objects.get(id=pid)
-    return HttpResponse(
-            
-    f" <p>id: {pizza.id}</p> <p>title: {pizza.title}</p><p> description: {pizza.description}</p>"
-            
-    )
+    try:
+        pizza = Pizza.objects.get(id=pid)
+        return HttpResponse( f" <p>id: {pizza.id}</p> <p>title: {pizza.title}</p><p> description: {pizza.description}</p>" )
+    except:
+        return HttpResponse(f"<p>status: error</p> <p>message: pizza_id={pid} not found</p>")
+
+def random(request):
+    try:
+        pid = randint(1,6)
+        pizza = Pizza.objects.get(id=pid)
+        return HttpResponse( f" <p>id: {pizza.id}</p> <p>title: {pizza.title}</p><p> description: {pizza.description}</p>" )
+    except:
+        return HttpResponse(f"<p>status: error</p> <p>message: pizza_id={pid} not found</p>")
+
+    
+
+
+    
